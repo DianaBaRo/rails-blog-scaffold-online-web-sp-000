9049f519648334770da0226f9feea8f8e3b452d5
Diana Badas <diana.badas@gmail.com>
2019-05-24 15:03:28 +0000

    Automatically backed up by Learn

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,16 +1,1 @@
-require 'rails_helper'
 
-RSpec.describe "posts/new", :type => :view do
-  before(:each) do
-    assign(:post, Post.new(
-      :title => "MyString"
-    ))
-  end
-
-  it "renders new post form" do
-    render
-
-  
-    end
-  end
-end
